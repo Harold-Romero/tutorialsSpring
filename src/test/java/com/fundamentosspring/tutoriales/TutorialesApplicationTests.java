3f0fbd9599a7fcbc0416093d612b61239565c35f
Harold Romero <harold.romero07@gmail.com>
2021-11-17 16:53:03 -0500

    first commit

---
+++ src/test/java/com/fundamentosspring/tutoriales/TutorialesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosspring.tutoriales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
